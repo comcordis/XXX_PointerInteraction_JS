32e9eca4f808ccc5ff557b96039fd249ef25c3c0
vinceMeens <v.w.a.meens@comcordis.com>
2013-09-11 01:26:23 +0200

    Static publishing per project

---
+++ publish.static.php
@@ -0,0 +1,14 @@
+<?php
+
+XXX_Static_Publisher::publishMergedFilesFromOtherProject($project, $deployIdentifier, array
+(
+	'XXX_PointerInteraction_CombinedMouseOver.js',
+	'XXX_PointerInteraction_DragAndDrop.js',
+	'XXX_PointerInteraction_DragCircleWithin.js',
+	'XXX_PointerInteraction_DragOffset.js',
+	'XXX_PointerInteraction_DragWithin.js',
+	'XXX_PointerInteraction_MouseOverWithin.js'
+),
+'initialize.js');
+
+?>
